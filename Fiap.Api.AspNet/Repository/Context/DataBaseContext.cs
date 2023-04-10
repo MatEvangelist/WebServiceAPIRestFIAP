469800e656d34e30bd6b6e65ff78c6932961d70f
Mathews Evangelista <mathews.evangelista@yaman.com.br>
2023-04-10 19:29:31 -0300

    Criação do repośitório, fork na atividade do capítulo 5

---
+++ Fiap.Api.AspNet/Repository/Context/DataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Fiap.Api.AspNet.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Fiap.Api.AspNet.Repository.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        // Propriedade que será responsável pelo acesso a tabela de Representantes
+        public DbSet<RepresentanteModel> Representante { get; set; }
+
+        // Propriedade que será responsável pelo acesso a tabela de Cliente
+        public DbSet<ClienteModel> Cliente { get; set; }
+
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DataBaseContext()
+        {
+        }
+
+    }
+}
---
+++ Fiap.Api.AspNet/Repository/RepresentanteRepository.cs
@@ -0,0 +1,105 @@
+﻿using Fiap.Api.AspNet.Models;
+using Fiap.Api.AspNet.Repository.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Api.AspNet.Repository
+{
+    public class RepresentanteRepository
+    {
+        private readonly DataBaseContext dataBaseContext;
+
+        public RepresentanteRepository(DataBaseContext ctx)
+        {
+            dataBaseContext = ctx;
+        }
+
+        public IList<RepresentanteModel> Listar()
+        {
+            var lista = new List<RepresentanteModel>();
+            lista = dataBaseContext.Representante.ToList<RepresentanteModel>();
+            return lista;
+        }
+
+        public IList<RepresentanteModel> ListarRepresentantesComClientes()
+        {
+
+            var lista = new List<RepresentanteModel>();
+
+            lista = dataBaseContext.Representante
+                    .Include(r => r.Clientes)
+                        .ToList<RepresentanteModel>();
+
+            return lista;
+        }
+
+
+        public IList<RepresentanteModel> ListarOrdenadoPorNome()
+        {
+            var lista = new List<RepresentanteModel>();
+
+            lista = dataBaseContext
+                .Representante
+                .OrderBy(r => r.NomeRepresentante).ToList<RepresentanteModel>();
+
+            return lista;
+        }
+
+        public IList<RepresentanteModel> ListarOrdenadoPorNomeDescendente()
+        {
+            var lista = new List<RepresentanteModel>();
+
+            lista = dataBaseContext
+                .Representante
+                .OrderByDescending(r => r.NomeRepresentante).ToList<RepresentanteModel>();
+
+            return lista;
+        }
+
+
+        public RepresentanteModel ConsultarPorCpf(String cpf)
+        {
+            var representante = dataBaseContext.Representante.
+                    Where(r => r.Cpf == cpf).
+                        FirstOrDefault<RepresentanteModel>();
+
+            return representante;
+        }
+
+        public RepresentanteModel ConsultarPorParteNome(String nomeParcial)
+        {
+            var representante = dataBaseContext.Representante.
+                    Where(r => r.NomeRepresentante.Contains(nomeParcial)).
+                        FirstOrDefault<RepresentanteModel>();
+
+            return representante;
+        }
+
+        public RepresentanteModel Consultar(int id)
+        {
+            var representante = dataBaseContext.Representante.Find(id);
+
+            return representante;
+        }
+
+        public void Inserir(RepresentanteModel representante)
+        {
+            dataBaseContext.Representante.Add(representante);
+            dataBaseContext.SaveChanges();
+        }
+
+        public void Alterar(RepresentanteModel representante)
+        {
+            dataBaseContext.Representante.Update(representante);
+            dataBaseContext.SaveChanges();
+        }
+
+        public void Excluir(int id)
+        {
+            var representante = new RepresentanteModel(id, "");
+
+            dataBaseContext.Representante.Remove(representante);
+            dataBaseContext.SaveChanges();
+        }
+
+    }
+}
